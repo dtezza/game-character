ded62e75d2539b74b5167e13e47dbdb3b93273d7
dtezza <36766114+dtezza@users.noreply.github.com>
2021-02-12 20:21:38 -0600

    Create gameCharacter.h

---
+++ gameCharacter.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "boot.h"
+#include "armor.h"
+#include "helmet.h"
+#include "pants.h"
+#include "gloves.h"
+#include "weapon.h"
+
+using namespace std;
+
+class GameCharacter {
+    private:
+    
+    public:
+ 
+};
